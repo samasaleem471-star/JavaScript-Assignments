2eaeb2b64b40f03a6b8b67aab071c2c781273616
samasaleem471 <samasaleem471@gmail.com>
2026-02-21 14:06:11 +0500

    1 commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// Question1
+// alert("Welcome to our Website!")
+// Question2
+// alert("Error! please enter a valid password.")
+// Question3
+// alert("Welcome to JS Land... \n Happy Coding! ")
+// Question4
+// alert("Welcome to JS Land...")
+// alert("Happy Coding! \n Prevent this page from creating additional dialogs.")
+// Question5
+// alert("Hello... I can run JS through myweb browser's console")
