12b78e38c473ccebf4774ab25246f011596ce85f
samasaleem471 <samasaleem471@gmail.com>
2026-02-27 22:35:04 +0500

    done

---
+++ Assignment 2/app.js
@@ -0,0 +1,43 @@
+// Question 1
+// let userName;
+
+// Question 2
+// let studentName = "Samaa Saleem"
+
+// Question 3
+
+//  a)
+// let message 
+
+// b) 
+// message = "Hello World"
+
+// c)
+// alert(message)
+
+// Question 4
+// let studentName ="John Doe"
+// let studentAge = 15
+// let studentCourse = "Certified Mobile Application Development"
+
+// alert(studentName)
+// alert(studentAge + " years old")
+// alert(studentCourse)
+
+// Question 5
+// let pizza ="PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(pizza)
+
+// Question 6
+// let email ="samasaleem@gmail.com"
+// alert("My email address is "+ email)
+
+// Question 7 
+// let book = "A smarter way to learn JavaScript"
+// alert("I am trying to lern from the Book "+ book)
+
+// Question 8
+// let javaScript= "JavaScript"
+// document.write("Yah! I can write HTML content through "+ javaScript)
+
+// Queston 9
