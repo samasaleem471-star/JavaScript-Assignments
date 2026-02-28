5f3d388b0d3846d197ca1dedf3c83b8e6b57f549
samasaleem471 <samasaleem471@gmail.com>
2026-02-28 13:25:49 +0500

    3done

---
+++ Assignment 3/app.js
@@ -0,0 +1,20 @@
+// Question # 01
+// let age =15
+// alert("I am "+age +" years old")
+
+// Question # 02
+// let visitor;
+// visitor =14
+// alert("You have visited this site "+visitor+" times")
+
+// Question # 03 
+// let birthYear =2009
+// document.write("My birth year is "+birthYear)
+// document.write("<br />")
+// document.write("Data type of my declared variable is number")
+
+// Question # 04
+let visitorName = "John Doe"
+let productName = "T-shirt"
+let quantity =5
+document.write(visitorName+" ordered "+quantity+" "+productName+"(s)"+" on XYZ Clothing store.")
