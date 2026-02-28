04cd9da2fee778806178c05c5d939c06997f4299
samasaleem471 <samasaleem471@gmail.com>
2026-02-28 14:00:31 +0500

    4done

---
+++ Assignment 4/app.js
@@ -0,0 +1,34 @@
+// Question # 01
+let myName, myCourse, myAge;
+
+// Question # 02
+
+// LEGAL VARIABLES
+// let studentName
+// let student_name
+// let student$name
+// let _studentName
+// let studentName49
+
+// ILLEGAL VARIABLES
+// let let  
+// let student name 
+// let 2studentName
+// let @student 
+// let const
+
+// Question # 03
+// let blank1="Rules for naming JS variables"
+// let blank2="numbers, _, $, letters"
+// let blank3="letters, _ or $"
+// let blank4="sensitive"
+// let blank5="keywords"
+//  document.write(blank1)
+//  document.write("<br />")
+//  document.write("Variable names can only contain "+blank2)
+//  document.write("<br />")
+//  document.write("Variables must begin with a"+blank3)
+//  document.write("<br />")
+//  document.write("Variable names are case "+ blank4)
+//  document.write("<br />")
+//  document.write("Variable names should not be JS "+ blank5)
