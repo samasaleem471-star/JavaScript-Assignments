1ef9aa8822af5a94271f5f62d16f25fbc8ee4c4e
samasaleem471 <samasaleem471@gmail.com>
2026-03-07 13:32:20 +0500

    5done

---
+++ Assignment 5/app.js
@@ -0,0 +1,107 @@
+// Question # 01
+// let num1 = +prompt("Enter the number")
+// let num2 = +prompt("Enter the number")
+// let num = (num1+num2)
+// document.write(`Sum of ${num1} and ${num2} is ${num}`)
+
+// Question # 02 
+
+// let num1 = prompt("Enter the number")
+// let num2 = prompt("Enter the number")
+// let num = (num1 - num2)
+// document.write(`Difference of ${num1} and ${num2} is ${num}`)
+
+
+// let num1 = prompt("Enter the number")
+// let num2 = prompt("Enter the number")
+// let num = (num1 * num2)
+// document.write(`Product of ${num1} and ${num2} is ${num}`)
+
+// let num1 = prompt("Enter the number")
+// let num2 = prompt("Enter the number")
+// let num = (num1 / num2)
+// document.write(`Quotient of ${num1} and ${num2} is ${num}`)
+
+// Question # 03
+
+// a 
+// let num;
+
+// b 
+// document.write(`Value after variable declaration is ${num}`)
+// document.write("<br />")
+
+// c 
+// num = 5;
+
+// d
+// document.write(`Initial value is ${num}`)
+// document.write("<br />")
+
+// e 
+// num++;
+
+// f 
+// document.write(`Value after increment is ${num}`)
+// document.write("<br />")
+
+// g 
+// num = num + 7
+
+// h 
+// document.write(`Value after addition is ${num}`)
+// document.write("<br />")
+
+// i 
+// num--;
+
+// j 
+// document.write(`Value after decrement is ${num}`)
+// document.write("<br />")
+
+// k 
+// num= num/3
+// document.write(`The remainder is 0`)
+// document.write("<br />")
+
+// Question # 04
+// let cost = 600
+// let totalCost = cost*5
+// document.write(`Total cost to buy 5 tickets to a movie is ${totalCost} PKR`)
+
+// Question # 05
+// let count = 1;
+// let num = 4
+// document.write("Table of 4")
+// document.write("<br />")
+// document.write(`${num} x ${count} = ${num}`)
+// document.write("<br />")
+// count++
+// document.write(`${num} x ${count} = ${num*count}`)
+// document.write("<br />")
+// count++
+// document.write(`${num} x ${count} = ${num*count}`)
+// document.write("<br />")
+// count++
+// document.write(`${num} x ${count} = ${num*count}`)
+// document.write("<br />")
+// count++
+// document.write(`${num} x ${count} = ${num*count}`)
+// document.write("<br />")
+// count++
+// document.write(`${num} x ${count} = ${num*count}`)
+// document.write("<br />")
+// count++
+// document.write(`${num} x ${count} = ${num*count}`)
+// document.write("<br />")
+// count++
+// document.write(`${num} x ${count} = ${num*count}`)
+// document.write("<br />")
+// count++
+// document.write(`${num} x ${count} = ${num*count}`)
+// document.write("<br />")
+// count++
+// document.write(`${num} x ${count} = ${num*count}`)
+
+
+
