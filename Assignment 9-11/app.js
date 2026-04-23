d44b5556aeea2f11970ac8aaa695f55b57796f82
samasaleem471 <samasaleem471@gmail.com>
2026-04-23 22:22:35 +0500

    9-11done

---
+++ Assignment 9-11/app.js
@@ -0,0 +1,87 @@
+// Question # 01
+// let city = prompt("Where do you live?");
+// if (city === "Karachi") {
+//   alert("Welcome to city of lights");
+// }
+
+// Question # 02
+// let gender = prompt("What's your gender?");
+// if (gender === "female") {
+//   alert("Good Morning Ma'am");
+// } else if (gender === "male") {
+//   alert("Good Morning Sir");
+// }
+
+// Question # 03 
+// let signalColor = prompt("What's the color of road traffic signal?");
+// if (signalColor === "Red"){
+//     alert("Must Stop")
+// }else if(signalColor === "Yellow"){
+//     alert("Ready to move")
+// }else if(signalColor === "Green"){
+//     alert("Move now")
+// }
+
+// Question # 04
+// let fuel = prompt("What's your fuel level (in litres)?");
+// if(fuel <br "0.25litres"){
+//     alert("Please refill the fuel in your car?")
+// }
+
+// Question # 05
+document.write("Question 05")
+// "a"
+// let a =4;
+// if(++a ===5){
+//     alert("given condition for variable a is true");
+// }
+// document.write("</br>")
+// document.write("a : Displayed")
+
+// "b"
+// let b = 82;
+// if(b++ === 83){
+//     alert("given condition for variable b is true");
+// }
+// document.write("</br>")
+// document.write("b : Not Displayed")
+
+// "c"
+// let c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// document.write("c : Condition 2 or 4 Displayed")
+
+// "d"
+// let materialCost = 20000;
+// let laborCost = 2000;
+// let totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+//     alert("The cost equals")
+// }
+// document.write("d : Displayed")
+
+// "e"
+// if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+// document.write("e : Displayed")
+
+// "f"
+// if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+// document.write("e : Displayed")
