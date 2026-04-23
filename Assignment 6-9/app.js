0a13b9edcc275f84d08f8a1d3c0bd18dbec4557a
samasaleem471 <samasaleem471@gmail.com>
2026-04-23 20:27:38 +0500

    Done

---
+++ Assignment 6-9/app.js
@@ -0,0 +1,34 @@
+// Question # 01
+let num = 10 
+document.write(`The value of a is: ${num}` )
+document.write("</br></br>")
+
+// Question # 02
+document.write(`The value of ++a is: ${++num}` )
+document.write("</br>")
+document.write(`Now the value of a is: ${num}` )
+document.write("</br></br></br>")
+
+// Question # 03 
+document.write(`The value of a++ is: ${num++}` )
+document.write("</br>")
+document.write(`Now the value of a is: ${num}` )
+document.write("</br></br></br>")
+
+
+// Question # 04 
+document.write(`The value of --a is: ${--num}` )
+document.write("</br>")
+document.write(`Now the value of a is: ${num}` )
+document.write("</br></br></br>")
+
+
+// Question # 05
+document.write(`The value of a-- is: ${num--}` )
+document.write("</br>")
+document.write(`Now the value of a is: ${num}` )
+document.write("</br></br></br>")
+
+
+
+
