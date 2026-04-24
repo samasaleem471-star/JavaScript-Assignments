6bf73c6d8e2aa319fbbc38c05f9e3fb5baa5da2f
samasaleem471 <samasaleem471@gmail.com>
2026-04-24 17:45:13 +0500

    done

--- Assignment 12-13/app.js
+++ Assignment 12-13/app.js
@@ -1,1 +1,63 @@
-// Question # 01
+// // Question # 01
+// let ch = prompt("Enter a character");
+// let code = ch.charCodeAt(0)
+// if(code >= 48 && code <=57){
+//     alert("This is a number")
+// }else if(code >= 67 && code <=90){
+//     alert("This is a uppercase")
+// }else if(code >= 97 && code <=122){
+//     alert("This is a lowercase")
+// }
+
+// Questuion # 02
+// let num1 = +prompt("Enter first number");
+// let num2 = +prompt("Enter second number");
+
+// if(num1 > num2){
+//     alert(`The larger number is ${num1}`)
+// }else if(num1 < num2){
+//     alert(`The larger number is ${num2}`)
+// }else{
+//     alert("Both integars are equal")
+// }
+
+// Question # 03
+// let num = +prompt("Enter a number");
+// if (num > 0) {
+//   alert("The number is positive");
+// } else if (num < 0) {
+//   alert("The number is negative");
+// } else if (num === 0) {
+//   alert("The number is zero");
+// }
+
+// Question # 04
+// let num = prompt("Enter a character");
+// if(num === "a" || num === "A" || num === "e" || num === "E" || num === "i" || num === "I" || num === "o" || num === "O" || num === "u" || num === "U" ){
+//     alert("True")
+// }else{
+//     alert("False")
+// }
+
+// Question # 05
+// let password = "javascript";
+// let userPassword = prompt("Enter your password")
+// if(userPassword === null || userPassword === ""){
+//     alert("Please enter your password")
+// }else if(password === userPassword){
+//     alert("Correct! The password you entered matches the original password")
+// }else{
+//     alert("Incorrect password")
+// }
+
+// Question # 06
+// let greeting;
+// let hour = 13;
+// if(hour < 18){
+//     greeting ="Good Day"
+// }else{
+//     greeting ="Good Evening"
+// }
+
+// Question # 07
+// smjh nhi aaya 
