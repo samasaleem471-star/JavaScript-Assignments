90679a9482b6e196f7174e282245b116ed80af3e
samasaleem471 <samasaleem471@gmail.com>
2026-04-25 00:36:47 +0500

    done

---
+++ Assignment 13-14/app.js
@@ -0,0 +1,79 @@
+// Question # 01
+// let student = []
+
+// Question # 02
+// let student = []
+
+// Question # 03
+// let studentName ;
+// studentName = ["Sam","hania"];
+
+// Question # 04
+// let studentNum ;
+// studentNum = [3,3];
+
+// Question # 05
+// let boolean ;
+// boolean = ["true","false"];
+
+// Question # 06
+// let mixed ;
+// mixed = ["true",1,"sam"];
+// console.log(mixed)
+
+// // Question # 07
+// let qualification ;
+// qualification = ["SSC","HSC","BCS","BS","BCOM","MS","M.Phil","PhD"]
+// document.write("Qualifications:" + "</br></br>")
+// document.write("1)" + qualification[0] + "</br>")
+// document.write("2)" + qualification[1] + "</br>")
+// document.write("3)" + qualification[2] + "</br>")
+// document.write("4)" + qualification[3] + "</br>")
+// document.write("5)" + qualification[4] + "</br>")
+// document.write("6)" + qualification[5] + "</br>")
+// document.write("7)" + qualification[6] + "</br>")
+// document.write("8)" + qualification[7] + "</br>")
+
+// // Question # 08
+// let studentName =["Michael","John","Tony"]
+// let studentMarks =[320,230,480]
+// let totalMarks = 500 ;
+// let studentPercentage =[(studentMarks[0]/totalMarks)*100 , (studentMarks[1]/totalMarks)*100 , (studentMarks[2]/totalMarks)*100]
+// document.write(`Score of ${studentName[0]} is ${studentMarks[0]}. Percentage: ${studentPercentage[0]}% </br>`)
+// document.write(`Score of ${studentName[1]} is ${studentMarks[1]}. Percentage: ${studentPercentage[1]}% </br>`)
+// document.write(`Score of ${studentName[2]} is ${studentMarks[2]}. Percentage: ${studentPercentage[2]}% </br>`)
+
+// // Question # 09
+let color =["pink","green","orange","purple"]
+document.write(color)
+
+// "a"
+// document.write("</br>")
+// let userColor = prompt("Add your color at the start")
+// color.unshift(userColor)
+// document.write(color)
+
+// "b"
+// document.write("</br>")
+// let userColor = prompt("Add your color at the end")
+// color.push(userColor)
+// document.write(color)
+
+// "c"
+// document.write("</br>")
+// color.unshift("black","blue")
+// document.write(color)
+
+// "d"
+// document.write("</br>")
+// color.shift()
+// document.write(color)
+
+// "e"
+// document.write("</br>")
+// color.pop()
+// document.write(color)
+
+// smjh nhi aa rha iske aage
+
+
